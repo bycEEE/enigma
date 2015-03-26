5699ce37e2ee3fa457b7fbc1b1801a5df1ca1648
Brian Choy <bycEEE@gmail.com>
2015-03-25 23:54:56 -0400

    Add CLI

--- enigma.rb
+++ enigma.rb
@@ -1,3 +1,7 @@
+require_relative 'machine'
+require_relative 'plugboard'
+require_relative 'rotor'
+require_relative 'reflector'
 class Enigma
   def call
     puts "Welcome to the Enigma Machine"
@@ -9,31 +13,80 @@ class Enigma
   end
 
   def run
-    print "Phrase to encrypt: "
+    print "Please select an option: "
     input = get_user_input
-    if input == "help"
+
+    case input
+    when "encrypt"
+      encrypt
+    when "decrypt"
+      decrypt
+    when "generate rotor"
+      generate_rotor
+    when "generate reflector"
+      generate_reflector
+    when "generate plugboard"
+      generate_plugboard
+    when "help"
       help
-    elsif input == "exit"
+    when "exit"
       exit
     else
-      search(input)
+      puts "Invalid input"
     end
-    run
   end
 
-  def search(input)
-    search_term = input.split(" ")[0]
-    puts "Your search term was #{search_term}, I am searching..."
-    url = "https://twitter.com/search?q=#{search_term}&src=typd&f=realtime"
-    tweet = ExampleScraper.new(url).example_method.sample
-    puts "Thank you for your patience. I found this on Twitter:"
-    puts tweet.example
+  def encrypt
+    print "Input phrase to encrypt: "
+    input = get_user_input.upcase
+    my_enigma = Machine.new
+    puts "Your encrypted phrase is: #{my_enigma.encrypt(input)}"
+  end
+
+  def decrypt
+    print "Input phrase to decrypt: "
+    input = get_user_input.upcase
+    my_enigma = Machine.new
+    puts "Your decrypted phrase is: #{my_enigma.encrypt(input)}"
+  end
+
+  def generate_rotor
+    print "Select which rotor to generate (1-5). Type 'all' to generate all rotors"
+    input = get_user_input
+
+    if input.is_a? Fixnum
+      Rotor.generate_rotor(input)
+      puts "Rotor successfully generated"
+    elsif input == "all"
+      Rotor.generate_rotor(1)
+      Rotor.generate_rotor(2)
+      Rotor.generate_rotor(3)
+      Rotor.generate_rotor(4)
+      Rotor.generate_rotor(5)
+      puts "Rotors successfully generated"
+    else
+      puts "Invalid input"
+    end
+  end
+
+  def generate_reflector
+    Reflector.generate_reflector
+    puts "Reflector generated"
+  end
+
+  def generate_plugboard
+    Plugboard.generate_plugboard
+    puts "Plugboard generated"
   end
 
   def help
+    puts "Type 'encrypt' to encrypt your message"
+    puts "Type 'decrypt' to decrypt your message"
+    puts "Type 'generate rotor' followed by a number (1-5) to generate that rotor"
+    puts "Type 'generate reflector' to generate a reflector"
     puts "Type 'exit' to exit"
     puts "Type 'help' to view this menu again"
-    puts "Type anything else to search for a Tweet"
   end
-
 end
+
+Enigma.new.call
--- machine.rb
+++ machine.rb
@@ -13,12 +13,9 @@ class Machine
     @reflector = Reflector.new
   end
 
-  def input(string)
-    # rotor_fast, rotor_medium, rotor_slow = @rotor_fast.dup, @rotor_medium.dup, @rotor_slow.dup
-    # plugboard = @plugboard.dup
-    # reflector = @reflector.dup
-
-    string.chars.map do |char|
+  def encrypt(input)
+    output = 
+    input.chars.map do |char|
       self.rotor_fast = rotor_fast.rotate
       self.rotor_medium = rotor_medium.rotate if rotor_fast.offset % 25 == 0
       self.rotor_slow = rotor_slow.rotate if rotor_medium.offset % 25*25 == 0
@@ -38,10 +35,17 @@ class Machine
       plugboard.inverse_translate(char).nil? ? char : plugboard.inverse_translate(char)
     end.join
   end
+
+  def print_message(message)
+    puts message
+  end
 end
 
-machine = Machine.new
-text = 'HIMOMTHISISMYENIGMAMACHINE'
-puts "#{text} has been encrypted to #{encrypted = machine.input(text)}"
-machine = Machine.new
-puts "#{encrypted} has been decrypted to #{machine.input(encrypted)}"
+# machine = Machine.new
+# text = 'HIMOMTHISISMYENIGMAMACHINE'
+# puts "#{text} has been encrypted to #{encrypted = machine.input(text)}"
+# machine = Machine.new
+# puts "#{encrypted} has been decrypted to #{machine.input(encrypted)}"
+
+# HIMOMTHISISMYENIGMAMACHINE has been encrypted to BTFDYXTCZRZLCJSRDLHFJDUREO
+# BTFDYXTCZRZLCJSRDLHFJDUREO has been decrypted to HIMOMTHISISMYENIGMAMACHINE
