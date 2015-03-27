23274e7dc3b15899f3bee175cfb99e917d25e505
Brian Choy <bycEEE@gmail.com>
2015-03-26 21:27:15 -0400

    Refactor encrypt method

--- machine.rb
+++ machine.rb
@@ -14,21 +14,7 @@ class Machine
 
   def encrypt(input)
     input.chars.map do |char|
-      rotate_rotors
-
-      char = plugboard.translate(char).nil? ? char : plugboard.translate(char)
-
-      char = self.rotor_fast.translate(char)
-      char = self.rotor_medium.translate(char)
-      char = self.rotor_slow.translate(char)
-   
-      char = reflector.translate(char)
-   
-      char = self.rotor_slow.inverse_translate(char)
-      char = self.rotor_medium.inverse_translate(char)
-      char = self.rotor_fast.inverse_translate(char)
-   
-      plugboard.inverse_translate(char).nil? ? char : plugboard.inverse_translate(char)
+      encrypt_char(char)
     end.join
   end
 
@@ -37,18 +23,34 @@ class Machine
   end
 
   private
+  def encrypt_char(char)
+    rotate_rotors
+    char = plugboard.translate(char).nil? ? char : plugboard.translate(char)
+    char = encrypt_through_rotor(char)
+    char = reflector.translate(char)
+    char = encrypt_through_rotor_inverse(char)
+    plugboard.inverse_translate(char).nil? ? char : plugboard.inverse_translate(char)
+  end
+
   def rotate_rotors
     self.rotor_fast = rotor_fast.rotate
     self.rotor_medium = rotor_medium.rotate if rotor_fast.offset % 25 == 0
     self.rotor_slow = rotor_slow.rotate if rotor_medium.offset % 25*25 == 0
   end
+
+  def encrypt_through_rotor(char)
+    char = self.rotor_fast.translate(char)
+    char = self.rotor_medium.translate(char)
+    char = self.rotor_slow.translate(char)
+  end
+
+  def encrypt_through_rotor_inverse(char)
+    char = self.rotor_slow.inverse_translate(char)
+    char = self.rotor_medium.inverse_translate(char)
+    char = self.rotor_fast.inverse_translate(char)
+  end
 end
 
-# machine = Machine.new
-# text = 'HIMOMTHISISMYENIGMAMACHINE'
-# puts "#{text} has been encrypted to #{encrypted = machine.input(text)}"
-# machine = Machine.new
-# puts "#{encrypted} has been decrypted to #{machine.input(encrypted)}"
-
+# ROTORS 1 2 3 | 21 3 11
 # HIMOMTHISISMYENIGMAMACHINE has been encrypted to BTFDYXTCZRZLCJSRDLHFJDUREO
 # BTFDYXTCZRZLCJSRDLHFJDUREO has been decrypted to HIMOMTHISISMYENIGMAMACHINE
